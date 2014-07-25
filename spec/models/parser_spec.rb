33cb0d429c9705b24cf24c01658b39aac64b40ab
Rajnish <dashora.rajnish@gmail.com>
2014-07-25 10:58:56 +0530

    parsing logic for base operations added

---
+++ spec/models/parser_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+describe 'Parser' do
+
+
+ let (:calculator){Calculator.new({:state => 5})}
+ let (:parser) {Parser.new(calculator)}
+ context '#parse' do
+
+   context '#addition' do
+    it 'should perform operation on "add 5" and return 10  'do
+      parser.perform_operation("add 5")
+      expect(calculator.state).to eq(10)
+    end
+
+   end
+
+   context '#subtraction' do
+
+    it 'should perform operation on "sub 3" and return 2'do
+      parser.perform_operation("sub 3")
+      expect(calculator.state).to eq(2)
+    end
+
+   end
+
+   context '#multiplication' do
+
+    it 'should perform operation on "mul 5" and return 25' do
+      parser.perform_operation("mul 5")
+      expect(calculator.state).to eq(25)
+    end
+
+   end
+
+
+   context '#division' do
+    it 'should perform operation on "div 5" and return 1  'do
+      parser.perform_operation("div 5")
+      expect(calculator.state).to eq(1)
+    end
+
+   end
+ end
+
+
+
+end
